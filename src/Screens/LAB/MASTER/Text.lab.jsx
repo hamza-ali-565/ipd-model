5b8915f5f9f15ec8b2a2cf946739976d4ca87fe6
hamza-ali-565 <alimuhammadhamza402@gmail.com>
2024-08-01 22:56:42 +0500

    ranges shown successfully

--- src/Screens/LAB/MASTER/Text.lab.jsx
+++ src/Screens/LAB/MASTER/Text.lab.jsx
@@ -11,6 +11,33 @@ const LabTest = () => {
   const [CategoryData, setCategoryData] = useState([]);
   const [testTypeData, setTestTypeData] = useState([]);
   const [fontSizeData, setFontSizeData] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [rangeInfo, setRangeInfo] = useState([
+    {
+      equipment: "",
+      min: "",
+      max: "",
+      fromAge: "",
+      toAge: "",
+      unit: "",
+      normalRanges: "",
+      gender: "",
+      ageType: "",
+    },
+  ]);
+  const [previewInfo, setPreview] = useState([
+    {
+      equipment: "",
+      min: "",
+      max: "",
+      fromAge: "",
+      toAge: "",
+      unit: "",
+      normalRanges: "",
+      gender: "",
+      ageType: "",
+    },
+  ]);
   useEffect(() => {
     setDepartmentData([
       { name: "--" },
@@ -46,6 +73,119 @@ const LabTest = () => {
     ]);
   }, []);
 
+  const rangeData = [
+    {
+      EquipmentType: "",
+      gender: "",
+      min: 0,
+      max: 0,
+      fromAge: 0,
+      toAge: 0,
+      ageType: "",
+      normalRanges: "",
+    },
+  ];
+
+  /////
+
+  const handleInputChange = (e, index, field, type) => {
+    setErrorMessage("");
+    console.log(e);
+    let value;
+    if (type === "select") {
+      // Handle the case when the input is a dropdown/select
+      value = e;
+    } else {
+      // Handle other cases (text input, number input, etc.)
+      value = e.target.value;
+    }
+    setRangeInfo((prevRangeInfo) => {
+      const updatedItems = [...prevRangeInfo];
+      if (updatedItems[index]) {
+        const newItem = { ...updatedItems[index], [field]: value };
+        updatedItems[index] = newItem;
+      }
+      //   console.log(updatedItems);
+      //   setPreview(rangeInfo);
+      return updatedItems;
+    });
+  };
+
+  const prev = () => {
+    console.log(previewInfo);
+    const { ageType, fromAge, toAge, gender } = rangeInfo[0];
+    // Empty Fields Check
+    const params = ["equipment", "gender", "fromAge", "toAge", "ageType"];
+    for (const param of params) {
+      if (rangeInfo[0][param] === "") {
+        setErrorMessage(`${param} is required.`);
+        console.log(errorMessage);
+        return;
+      }
+    }
+    // age Greaters Check
+    if (toAge < fromAge) {
+      setErrorMessage("From Age must be less than or equal To Age.");
+    } else if (fromAge <= 0 || toAge <= 0) {
+      setErrorMessage("From Age And To Age Must be greater than 1.");
+    }
+    // duplicate Check
+    const duplicate = previewInfo.some(
+      (item) =>
+        item.gender === gender &&
+        item.fromAge === fromAge &&
+        item.toAge === toAge &&
+        item.ageType === ageType
+    );
+
+    if (duplicate) {
+      // Display your error message or take necessary action
+      console.error("Duplicate entry found!");
+      setErrorMessage("Gender With Same Age Group not Allowed Twise!");
+      return;
+    }
+    // avoiding first empty index but failed
+    if (Object.keys(previewInfo[0]).length === 0) {
+      setPreview(rangeInfo);
+    } else {
+      setPreview([...previewInfo, ...rangeInfo]);
+    }
+  };
+
+  //   const empty = () => {
+  //     setRangeInfo([
+  //       {
+  //         equipment: "",
+  //         min: "",
+  //         max: "",
+  //         fromAge: "",
+  //         toAge: "",
+  //         unit: "",
+  //         normalRanges: "",
+  //       },
+  //     ]);
+  //     setReportDays("");
+  //     setCategory("");
+  //     setDepartment("");
+  //     setTestName("");
+  //     setTestType("");
+  //     setPreview([
+  //       {
+  //         equipment: "",
+  //         min: "",
+  //         max: "",
+  //         fromAge: "",
+  //         toAge: "",
+  //         unit: "",
+  //         normalRanges: "",
+  //         gender: "",
+  //         ageType: "",
+  //       },
+  //     ]);
+  //   };
+
+  /////
+
   return (
     <div>
       <div className="bg-white bg-opacity-10 backdrop-blur-lg border border-white border-opacity-30 shadow-lg my-4 mx-4  p-3 rounded-3xl">
@@ -76,60 +216,125 @@ const LabTest = () => {
 
         <div className="bg-white bg-opacity-10 backdrop-blur-lg border border-white border-opacity-30 shadow-lg my-4 mx-4  p-3 rounded-3xl">
           <CenterHeading title={"TEST RANGES INFO"} />
-          <div className="flex items-center flex-col space-y-2 mt-3 md:grid md:grid-cols-2 md:justify-items-center">
-            <LabelledDropDown label={"Equipment Type"} data={fontSizeData} />
-            <LabelledDropDown label={"Gender"} data={fontSizeData} />
-          </div>
-          <div className="flex items-center flex-col space-y-2 mt-3 md:grid md:grid-cols-2 md:justify-items-center">
-            <LabeledInput label={"Min"} />
-            <LabeledInput label={"Max"} />
-          </div>
+          {rangeInfo.map((_, index) => (
+            <div>
+              <div className="flex items-center flex-col space-y-2 mt-3 md:grid md:grid-cols-2 md:justify-items-center">
+                <LabelledDropDown
+                  label={"Equipment Type"}
+                  data={fontSizeData}
+                  onChange={(selectedOption) =>
+                    handleInputChange(
+                      selectedOption,
+                      index,
+                      "equipment",
+                      "select"
+                    )
+                  }
+                />
+                <LabelledDropDown
+                  label={"Gender"}
+                  data={fontSizeData}
+                  onChange={(selectedOption) =>
+                    handleInputChange(selectedOption, index, "gender", "select")
+                  }
+                />
+              </div>
+              <div className="flex items-center flex-col space-y-2 mt-3 md:grid md:grid-cols-2 md:justify-items-center">
+                <LabeledInput
+                  label={"Min"}
+                  onChange={(e) => handleInputChange(e, index, "min")}
+                />
+                <LabeledInput
+                  label={"Max"}
+                  onChange={(e) => handleInputChange(e, index, "max")}
+                />
+              </div>
 
-          <div className="flex items-center flex-col space-y-2 mt-3 md:grid md:grid-cols-3 md:justify-items-center">
-            <LabeledInput label={"From Age"} />
-            <LabeledInput label={"To Age"} />
-            <LabelledDropDown label={"Age Type"} data={fontSizeData} />
-          </div>
-          <div className="flex items-center flex-col space-y-2 mt-3 md:grid md:grid-cols-2 md:justify-items-center">
-            <LabelledTextArea label={"Unit"} />
-            <LabelledTextArea label={"Normal Ranges"} />
-          </div>
-          <div className="flex justify-center mt-3">
-            <ButtonDis title={"Add"} />
-          </div>
-        </div>
+              <div className="flex items-center flex-col space-y-2 mt-3 md:grid md:grid-cols-3 md:justify-items-center">
+                <LabeledInput
+                  label={"From Age"}
+                  onChange={(e) => handleInputChange(e, index, "fromAge")}
+                />
+                <LabeledInput
+                  label={"To Age"}
+                  onChange={(e) => handleInputChange(e, index, "toAge")}
+                />
+                <LabelledDropDown
+                  label={"Age Type"}
+                  data={fontSizeData}
+                  onChange={(selectedOption) =>
+                    handleInputChange(
+                      selectedOption,
+                      index,
+                      "ageType",
+                      "select"
+                    )
+                  }
+                />
+              </div>
+              <div className="flex items-center flex-col space-y-2 mt-3 md:grid md:grid-cols-2 md:justify-items-center">
+                <LabelledTextArea
+                  label={"Unit"}
+                  onChange={(e) => handleInputChange(e, index, "unit")}
+                />
+                <LabelledTextArea
+                  label={"Normal Ranges"}
+                  onChange={(e) => handleInputChange(e, index, "normalRanges")}
+                />
+              </div>
+              <div className="flex justify-center mt-3">
+                <ButtonDis title={"Add"} onClick={prev} />
+              </div>
+            </div>
+          ))}
 
-        <div className="container mx-auto mt-3">
-          <div className="mt-3 grid grid-cols-11 text-xs font-bold justify-items-center items-center h-16 border border-gray-300">
-            <p>Equipment</p>
-            <p>Gender</p>
-            <p>Min</p>
-            <p>Max</p>
-            <p>Unit</p>
-            <p>From Age</p>
-            <p>To Age</p>
-            <p>Age Type</p>
-            <p>Normal Ranges</p>
-            <p>Update</p>
-            <p>Remove</p>
-          </div>
+          {errorMessage && (
+            <p className="flex justify-center font-bold text-red-600 text-xs md:text-sm">
+              *{errorMessage}*
+            </p>
+          )}
         </div>
-        {/* // */}
-        <div className="container mx-auto mt-3">
-          <div className="mt-3 grid grid-cols-11 text-xs justify-items-center items-center h-10 border border-gray-300">
-            <p>Equipment</p>
-            <p>Gender</p>
-            <p>Min</p>
-            <p>Max</p>
-            <p>Unit</p>
-            <p>From Age</p>
-            <p>To Age</p>
-            <p>Age Type</p>
-            <p>Normal Ranges</p>
-            <p>Update</p>
-            <p>Remove</p>
+        {previewInfo && (
+          <div>
+            <div className="container mx-auto mt-3">
+              <div className="mt-3 grid grid-cols-11 text-xs font-bold justify-items-center items-center h-16 border border-gray-300">
+                <p>Equipment</p>
+                <p>Gender</p>
+                <p>Min</p>
+                <p>Max</p>
+                <p>Unit</p>
+                <p>From Age</p>
+                <p>To Age</p>
+                <p>Age Type</p>
+                <p>Normal Ranges</p>
+                <p>Update</p>
+                <p>Remove</p>
+              </div>
+            </div>
+            {/* // */}
+            {previewInfo &&
+              previewInfo.map(
+                (items, i) =>
+                  items.equipment && (
+                    <div className="container mx-auto mt-3">
+                      <div className="mt-3 grid grid-cols-11 text-xs justify-items-center items-center h-10 border border-gray-300">
+                        {items.equipment}
+                        <p>{items.gender}</p>
+                        <p>{items.min}</p>
+                        <p>{items.max}</p>
+                        <p>{items.unit}</p>
+                        <p>{items.fromAge}</p>
+                        <p>{items.toAge}</p>
+                        <p>{items.ageType}</p>
+                        <p>{items.normalRanges.length > 0 ? "True" : "-"}</p>
+                        <p>Update</p>
+                        <p>Remove</p>
+                      </div>
+                    </div>
+                  )
+              )}
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
