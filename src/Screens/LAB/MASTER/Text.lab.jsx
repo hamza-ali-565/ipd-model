531e9b561d1fe327a6bb8adcc9cd78df4ba78cea
hamza-ali-565 <alimuhammadhamza402@gmail.com>
2024-08-01 21:03:32 +0500

    working on UI of Lab test creation

--- src/Screens/LAB/MASTER/Text.lab.jsx
+++ src/Screens/LAB/MASTER/Text.lab.jsx
@@ -3,6 +3,8 @@ import CenterHeading from "../../../Components/Center Heading/CenterHeading";
 import LabeledInput from "../../../Components/LabelledInput/LabeledInput";
 import LabelledDropDown from "../../../Components/LabelledDropDown/LabelledDropDown";
 import LabelledTextArea from "../../../Components/LabeledTextArea/LabelledTextArea";
+import moment from "moment";
+import ButtonDis from "../../../Components/Button/ButtonDis";
 
 const LabTest = () => {
   const [departmentData, setDepartmentData] = useState([]);
@@ -43,6 +45,7 @@ const LabTest = () => {
       { name: "16px" },
     ]);
   }, []);
+
   return (
     <div>
       <div className="bg-white bg-opacity-10 backdrop-blur-lg border border-white border-opacity-30 shadow-lg my-4 mx-4  p-3 rounded-3xl">
@@ -78,18 +81,53 @@ const LabTest = () => {
             <LabelledDropDown label={"Gender"} data={fontSizeData} />
           </div>
           <div className="flex items-center flex-col space-y-2 mt-3 md:grid md:grid-cols-2 md:justify-items-center">
-          <LabeledInput label={"Min"} />
-          <LabeledInput label={"Max"} />
+            <LabeledInput label={"Min"} />
+            <LabeledInput label={"Max"} />
           </div>
 
           <div className="flex items-center flex-col space-y-2 mt-3 md:grid md:grid-cols-3 md:justify-items-center">
-          <LabeledInput label={"From Age"} />
-          <LabeledInput label={"To Age"} />
-          <LabelledDropDown label={"Age Type"} data={fontSizeData} />
+            <LabeledInput label={"From Age"} />
+            <LabeledInput label={"To Age"} />
+            <LabelledDropDown label={"Age Type"} data={fontSizeData} />
           </div>
           <div className="flex items-center flex-col space-y-2 mt-3 md:grid md:grid-cols-2 md:justify-items-center">
-          <LabelledTextArea label={"Unit"} />
-          <LabelledTextArea label={"Normal Ranges"} />
+            <LabelledTextArea label={"Unit"} />
+            <LabelledTextArea label={"Normal Ranges"} />
+          </div>
+          <div className="flex justify-center mt-3">
+            <ButtonDis title={"Add"} />
+          </div>
+        </div>
+
+        <div className="container mx-auto mt-3">
+          <div className="mt-3 grid grid-cols-11 text-xs font-bold justify-items-center items-center h-16 border border-gray-300">
+            <p>Equipment</p>
+            <p>Gender</p>
+            <p>Min</p>
+            <p>Max</p>
+            <p>Unit</p>
+            <p>From Age</p>
+            <p>To Age</p>
+            <p>Age Type</p>
+            <p>Normal Ranges</p>
+            <p>Update</p>
+            <p>Remove</p>
+          </div>
+        </div>
+        {/* // */}
+        <div className="container mx-auto mt-3">
+          <div className="mt-3 grid grid-cols-11 text-xs justify-items-center items-center h-10 border border-gray-300">
+            <p>Equipment</p>
+            <p>Gender</p>
+            <p>Min</p>
+            <p>Max</p>
+            <p>Unit</p>
+            <p>From Age</p>
+            <p>To Age</p>
+            <p>Age Type</p>
+            <p>Normal Ranges</p>
+            <p>Update</p>
+            <p>Remove</p>
           </div>
         </div>
       </div>
